f7e75e9cc37e948f41a0eaf2b384937a03935cac
Brian <brian.w.tjahjadi@gmail.com>
2025-01-29 14:26:08 +1100

    renamed packages

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
 source /opt/ros/humble/setup.bash
-colcon build --packages-select keep-alive-test --symlink-install --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON 
+colcon build --packages-select keep-alive-estop --symlink-install --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON 
 source install/setup.bash
 
 # ros2 run keep-alive-test listener_node
