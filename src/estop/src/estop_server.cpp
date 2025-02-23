69f913707482b525b503292b75db31ffc25cad05
Brian <brian.w.tjahjadi@gmail.com>
2025-02-23 17:39:32 +1100

    added qos and wait for talker

--- src/estop/src/estop_server.cpp
+++ src/estop/src/estop_server.cpp
@@ -2,9 +2,13 @@
 
 EstopServer::EstopServer() : Node {"estop_server"}
 {
+  rmw_qos_profile_t estop_service_qos {};
+  estop_service_qos.reliability = RMW_QOS_POLICY_RELIABILITY_RELIABLE;
+
   // make service available across server
   service_ = this->create_service<estop_interfaces::srv::Estop>("estop", 
-    std::bind(&EstopServer::handle_estop_request, this, std::placeholders::_1, std::placeholders::_2)
+    std::bind(&EstopServer::handle_estop_request, this, std::placeholders::_1, std::placeholders::_2),
+    estop_service_qos
   );
   RCLCPP_INFO(this->get_logger(), "Ready to respond to Estop calls.");
 }
